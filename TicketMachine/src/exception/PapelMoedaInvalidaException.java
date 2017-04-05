8ebbe4de006a5663bdc833e955007639afd4d01e
lucastripoli <lucastripoli@192.168.1.5>
2017-04-04 21:07:48 -0300

    PapelMoedaInvalidaException CÒDIGO INCOMPLETO. #11

--- TicketMachine/src/exception/PapelMoedaInvalidaException.java
+++ TicketMachine/src/exception/PapelMoedaInvalidaException.java
@@ -3,7 +3,10 @@ package br.calebe.ticketmachine.exception;
 /**
  *
  * @author Calebe de Paula Bianchini
+ * PapelMoedaInvalidaException CÒDIGO INCOMPLETO. #11
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+    public PapelMoedaInvalidaException() {
+        super("Papel moeda Invalido");
+    }
 }
