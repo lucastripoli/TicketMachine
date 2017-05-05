034b6eccca214552218999adbad414c266967065
Familia <Familia@192.168.0.10>
2017-05-05 06:20:01 -0300

    Teste troco adicionado

---
+++ test/TrocoTest.java
@@ -0,0 +1,32 @@
+import br.calebe.ticketmachine.core.PapelMoeda;
+import br.calebe.ticketmachine.core.Troco.TrocoIterator;
+import br.calebe.ticketmachine.core.Troco;
+import java.util.Iterator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Familia
+ */
+public class TrocoTest {
+    
+
+
+
+  //   @Test
+   //  public void getIterator() {
+   //  Troco troco = new Troco(35);
+   //  Iterator<PapelMoeda> iterator;
+  //   iterator =troco.getIterator();
+     //    assertEquals(iterator, troco.getIterator());
+     //}
+     
+       //   @Test
+    // public void iterator() {
+   //  Troco troco = new Troco(550);
+   //  Iterator<PapelMoeda> iterator = troco.getIterator();
+    // boolean verifica = iterator.hasNext();
+    //     assertTrue(verifica == troco.getIterator().hasNext());
+    // }
+}
